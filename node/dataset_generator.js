5e5a725afde0055d3290295b4c405eebd8d60cd6
Sanjoth Rai <sanjr4279@gmail.com>
2024-03-11 23:17:42 +0530

    node canvas

--- node/dataset_generator.js
+++ node/dataset_generator.js
@@ -1,4 +1,9 @@
 const draw = require("../common/draw.js");
+
+const { createCanvas } = require("canvas");
+const canvas = createCanvas(400, 400);
+const ctx = canvas.getContext("2d");
+
 const constants = {};
 constants.DATA_DIR = "../data";
 constants.RAW_DIR = constants.DATA_DIR + "/raw";
@@ -38,6 +43,7 @@ fileNames.forEach((fn) => {
 fs.writeFileSync(constants.SAMPLES, JSON.stringify(samples));
 
 function generateImageFile(outputFile, paths) {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
     draw.paths(ctx, paths)
 
     const buffer = canvas.toBuffer("image/png");
